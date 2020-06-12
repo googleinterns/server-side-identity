6c8868d1ebb791c5d539657da2274880f8aca373
Stephen Otremba <sotremba007@gmail.com>
2020-06-12 13:36:15 -0700

    changed library name to gsi to fix existing name errors

--- google/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Google Sign-in namespace package."""
-import pkg_resources
-
-pkg_resources.declare_namespace(__name__)
-
--- gsi/verification/verifiers.py
+++ gsi/verification/verifiers.py
@@ -2,7 +2,7 @@ import json
 
 from six.moves import http_client
 
-import exceptions
+from bigtest.verification import exceptions
 #JWT Library
 
 # The URL that provides public certificates for verifying ID tokens issued
